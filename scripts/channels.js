127d86599072d5c9b4bfe27a2e052a31dcb659ad
edu_schenato <eduardo.schenato59@gmail.com>
2022-11-19 11:11:33 -0300

    Atualização playlists

--- scripts/channels.js
+++ scripts/channels.js
@@ -1,9 +1,17 @@
 var channels = [
-    { 
+    /*{ 
         name: "MLB",
         link: "mlb",
         url: "https://www.youtube.com/user/MLB",
         playlistId: "PLL-lmlkrmJanUePyXyLusrJGzyGRg-Qj3",
+        videos: [],
+        reverseOrder: false
+    },*/
+    { 
+        name: "NHL",
+        link: "nhl",
+        url: "https://www.youtube.com/c/NHL",
+        playlistId: "PL1NbHSfosBuG_HB8WTgFaCVsDWCcnOsUW",
         videos: [],
         reverseOrder: false
     },
@@ -15,43 +23,51 @@ var channels = [
             "PLRdw3IjKY2gmrC9ajF4ymE16kNsaLbKHt", 
             "PLRdw3IjKY2glPi-Vj2v7DHKVTkNQqJP2u", 
             "PLRdw3IjKY2gnCkpDA0W6nxtkdwDdoEDh7", 
-            "PLRdw3IjKY2gn6dxJ9XtcqzSMGi7glF7wY"
+            "PLRdw3IjKY2gn6dxJ9XtcqzSMGi7glF7wY", 
+            "PLRdw3IjKY2gnfBl7BQW3lmKSQeP79Nn4M", 
+            "PLRdw3IjKY2gni5pwzAt8q8a8kiVRVUp4J", 
+            "PLRdw3IjKY2gnEgIFCbkgFko4BqgmhpUav", 
+            "PLRdw3IjKY2gmRgju0urWGvGKHKAwcs4k9", 
+            "PLRdw3IjKY2gnRq5yLjsg_3pcVSYPYU06e", 
+            "PLRdw3IjKY2glEYjxFbzB2pLMIzW1LAdmX",  
+            "PLRdw3IjKY2gmi8j_FC1TDwLOkQoJhHlKw"
         ],
         videos: [],
         reverseOrder: true
     },
-    // { 
-    //     name: "NHL",
-    //     name: "nhl",
-    //     url: "https://www.youtube.com/c/NHL",
-    //     playlistId: "PL1NbHSfosBuG_HB8WTgFaCVsDWCcnOsUW",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
-    // { 
-    //     name: "NBA G-League",
-    //     link: "nba-g-league",
-    //     url: "https://www.youtube.com/c/nbagleague",
-    //     playlistId: "PLRF0Qy76Eqm7pvXG2vOJjli7NXowlG2y4",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
-    // { 
-    //     name: "WNBA",
-    //     link: "wnba",
-    //     url: "https://www.youtube.com/user/WNBA",
-    //     playlistId: "PLSPy3Xok-53TeqgYWdOIHV1OQLSdujVTJ",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
-    // { 
-    //     name: "EuroLeague",
-    //     link: "euroleague",
-    //     url: "https://www.youtube.com/c/euroleague",
-    //     playlistId: "PLXcjy-GNBj1zR7N7D32XsWbuzlU4uK5SS",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
+    { 
+        name: "NBA G-League",
+        link: "nba-g-league",
+        url: "https://www.youtube.com/c/nbagleague",
+        playlistId: "PLRF0Qy76Eqm4PM_B0Rrnk5_Z5b0bjnivI",
+        videos: [],
+        reverseOrder: false
+    },
+    /*{ 
+        name: "WNBA",
+        link: "wnba",
+        url: "https://www.youtube.com/user/WNBA",
+        playlistId: "PLSPy3Xok-53TeqgYWdOIHV1OQLSdujVTJ",
+        videos: [],
+        reverseOrder: false
+    },*/
+    { 
+        name: "EuroLeague",
+        link: "euroleague",
+        url: "https://www.youtube.com/c/euroleague",
+        playlistId: [
+            "PLXcjy-GNBj1xrDtDBVziMnDQRfk0KQT3s", 
+            "PLXcjy-GNBj1zbrd5PcUa-bLH0FONkP3sQ", 
+            "PLXcjy-GNBj1zcR1UgbBjx-gzhxlPNs2M5", 
+            "PLXcjy-GNBj1y5DM3XG3gAXRASEnFs3Sul", 
+            "PLXcjy-GNBj1wV69Wz3QN_MfyLPwZd4g3y", 
+            "PLXcjy-GNBj1z5kW2Zc-sW9fOOECLbsBc7", 
+            "PLXcjy-GNBj1zN25QnRltwIsQwMQHQO60Y",  
+            "PLXcjy-GNBj1ycP5Ue0jcCVPYe4ZcybgfJ"
+        ],
+        videos: [],
+        reverseOrder: true
+    },
     { 
         name: "NCAA Football",
         link: "ncaa-football",
@@ -60,22 +76,22 @@ var channels = [
         videos: [],
         reverseOrder: true
     },
-    // { 
-    //     name: "NCAA Baseball",
-    //     link: "ncaa-baseball",
-    //     url: "https://www.youtube.com/channel/UCi-INxdmFSrTrT95xU0sBpg",
-    //     playlistId: "PLBBzZXcG8cYBAk3WV2GpjPsKyPbEQmwzQ",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
-    // { 
-    //     name: "NCAA Softball",
-    //     link: "ncaa-softball",
-    //     url: "https://www.youtube.com/channel/UCi-INxdmFSrTrT95xU0sBpg",
-    //     playlistId: "PLBBzZXcG8cYAHMYn79OAzjMwSTrAevYNW",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
+    /*{ 
+        name: "NCAA Baseball",
+        link: "ncaa-baseball",
+        url: "https://www.youtube.com/channel/UCi-INxdmFSrTrT95xU0sBpg",
+        playlistId: "PLBBzZXcG8cYBAk3WV2GpjPsKyPbEQmwzQ",
+        videos: [],
+        reverseOrder: false
+    },*/
+    /*{ 
+        name: "NCAA Softball",
+        link: "ncaa-softball",
+        url: "https://www.youtube.com/channel/UCi-INxdmFSrTrT95xU0sBpg",
+        playlistId: "PLBBzZXcG8cYAHMYn79OAzjMwSTrAevYNW",
+        videos: [],
+        reverseOrder: false
+    },*/
     { 
         name: "CFL",
         link: "cfl",
@@ -84,30 +100,30 @@ var channels = [
         videos: [],
         reverseOrder: false
     },
-    { 
+    /*{ 
         name: "Premier League Lacrosse",
         link: "premier-league-lacrosse",
         url: "https://www.youtube.com/c/pll",
         playlistId: "PLYwxet-wh_uY2wWsnv93GM25zE4ucI4Ua",
         videos: [],
         reverseOrder: true
-    },
-    // { 
-    //     name: "LMB",
-    //     link: "lmb",
-    //     url: "https://www.youtube.com/c/LMBOficial",
-    //     playlistId: "PLRLm82cerwTcG58p8JBX7NCqk_6y8Qz9y",
-    //     videos: [],
-    //     reverseOrder: true
-    // },
-    { 
+    },*/
+    /*{ 
+        name: "LMB",
+        link: "lmb",
+        url: "https://www.youtube.com/c/LMBOficial",
+        playlistId: "PLRLm82cerwTcG58p8JBX7NCqk_6y8Qz9y",
+        videos: [],
+        reverseOrder: true
+    },*/
+    /*{ 
         name: "Super 12",
         link: "super-12",
         url: "https://www.youtube.com/c/rugbybrasil",
         playlistId: "PLW1ecmGi4AqYvQnlFbGSHNigwbTjlpLHz",
         videos: [],
         reverseOrder: true
-    },
+    },*/
     { 
         name: "Premiership Rugby",
         link: "premiership-rugby",
@@ -125,10 +141,15 @@ var channels = [
             "PLrYk229EnmBpHoXrAFeiRlEgE17r1ea8r", 
             "PLrYk229EnmBqjQSBI6NyfFHV5X79L8fQM", 
             "PLrYk229EnmBpfxFXtcnyF17EHqunMMLs7", 
-            "PLrYk229EnmBqyNnsFrKfxZcY_tjcKldtA"
+            "PLrYk229EnmBqyNnsFrKfxZcY_tjcKldtA", 
+            "PLrYk229EnmBrlRow9l9ZaXIJ5H0uqE7Jh", 
+            "PLrYk229EnmBo1mZNApRVg_C4JA0SptcsR", 
+            "PLrYk229EnmBrO4VtGfL40NrKtTQUGYGok", 
+            "PLrYk229EnmBqwI0i5AFWPG8H_lX6A2WvS", 
+            "PLrYk229EnmBrLrOgD4PzM7fu8CdfyZtQx"
         ],
         videos: [],
-        reverseOrder: false
+        reverseOrder: true
     },
     { 
         name: "United Rugby Championship",
@@ -136,7 +157,7 @@ var channels = [
         url: "https://www.youtube.com/c/UnitedRugbyChampionshipURC",
         playlistId: "PLclPnIJd8nRvun-WAP3ZFywyNFmp2UqR5",
         videos: [],
-        reverseOrder: true
+        reverseOrder: false
     },
     { 
         name: "Rugby Europe Super Cup",
@@ -146,116 +167,116 @@ var channels = [
         videos: [],
         reverseOrder: true
     },
-    // { 
-    //     name: "Super Rugby",
-    //     link: "super-rugby",
-    //     url: "https://www.youtube.com/user/SanzarRugby",
-    //     playlistId: "PL3Ypd5NbZr_G0EVSyhDyDg0Pc7j97an0i",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
-    // { 
-    //     name: "SLAR",
-    //     link: "slar",
-    //     url: "https://www.youtube.com/c/SLARTV",
-    //     playlistId: "PLa526XP1zbq332d0OHM_VXSn3xcWbbYnp",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
-    // { 
-    //     name: "Heineken Champions Cup",
-    //     link: "heineken-champions-cup",
-    //     url: "https://www.youtube.com/ChampionsCup",
-    //     playlistId: "PLPvhV-spCrYhaOTv2G8MrpHnrtlBlzjod",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
-    // { 
-    //     name: "European Rugby Challenge Cup",
-    //     link: "european-rugby-challenge-cup",
-    //     url: "https://www.youtube.com/ChampionsCup",
-    //     playlistId: "PLPvhV-spCrYgJC5BfIKaTFjEGyC3Wqnmh",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
-    { 
+    /*{ 
+        name: "Super Rugby",
+        link: "super-rugby",
+        url: "https://www.youtube.com/user/SanzarRugby",
+        playlistId: "PL3Ypd5NbZr_G0EVSyhDyDg0Pc7j97an0i",
+        videos: [],
+        reverseOrder: false
+    },*/
+    /*{ 
+        name: "SLAR",
+        link: "slar",
+        url: "https://www.youtube.com/c/SLARTV",
+        playlistId: "PLa526XP1zbq332d0OHM_VXSn3xcWbbYnp",
+        videos: [],
+        reverseOrder: false
+    },*/
+    /*{ 
+        name: "Heineken Champions Cup",
+        link: "heineken-champions-cup",
+        url: "https://www.youtube.com/ChampionsCup",
+        playlistId: "PLPvhV-spCrYhaOTv2G8MrpHnrtlBlzjod",
+        videos: [],
+        reverseOrder: false
+    },*/
+    /*{ 
+        name: "European Rugby Challenge Cup",
+        link: "european-rugby-challenge-cup",
+        url: "https://www.youtube.com/ChampionsCup",
+        playlistId: "PLPvhV-spCrYgJC5BfIKaTFjEGyC3Wqnmh",
+        videos: [],
+        reverseOrder: false
+    },*/
+    /*{ 
         name: "MLS",
         link: "mls",
         url: "https://www.youtube.com/c/mls",
         playlistId: "PLcj4z4KsbIoXqdWKX405un2PyALNClE8C",
         videos: [],
         reverseOrder: false
-    },
-    { 
+    },*/
+    /*{ 
         name: "USL Championship",
         link: "usl-championship",
         url: "https://www.youtube.com/USL",
         playlistId: "PL1Vshz9t5goWRgW-GRxTxpZg41ewoiqCb",
         videos: [],
         reverseOrder: false
-    },
-    { 
+    },*/
+    /*{ 
         name: "USL League One",
         link: "usl-league-one",
         url: "https://www.youtube.com/c/uslleague1",
         playlistId: "PLIui8vtIywxrAJsCOq4Yn2oQWoNIJLnb3",
         videos: [],
         reverseOrder: false
-    },
-    { 
+    },*/
+    /*{ 
         name: "Copa Libertadores",
         link: "copa-libertadores",
         url: "https://www.youtube.com/c/LibertadoresBR",
         playlistId: "PLuwL8egE4MmqpauXNMXJ7fcPw8wvHIyTq",
         videos: [],
         reverseOrder: false
-    },
-    { 
+    },*/
+    /*{ 
         name: "Copa Sul-Americana",
         link: "copa-sul-americana",
         url: "https://www.youtube.com/c/SudamericanaBR",
         playlistId: "PLfxJE7oOWiUn7AjG16BRJEhNJANK4mqRW",
         videos: [],
         reverseOrder: false
-    },
-    { 
+    },*/
+    /*{ 
         name: "AFC Champions League",
         link: "afc-champions-league",
         url: "https://www.youtube.com/c/TheAFCHub",
         playlistId: "PLPkuWEMcRg4GFWW8291B0S6gdLt3rPiYR",
         videos: [],
         reverseOrder: true
-    },
-    { 
+    },*/
+    /*{ 
         name: "AFC Cup",
         link: "afc-cup",
         url: "https://www.youtube.com/c/TheAFCHub",
         playlistId: "PLPkuWEMcRg4HUFX5HEUZvimudS9fnWVA-",
         videos: [],
         reverseOrder: true
-    }
-    // { 
-    //     name: "CONCACAF Champions League",
-    //     link: "concacaf-champions-league",
-    //     url: "https://www.youtube.com/c/concacaf",
-    //     playlistId: "PL6XTKrlgbQUBLDRjeCB4NLl0oqzTzODJV",
-    //     videos: [],
-    //     reverseOrder: false
-    // },
-    // { 
-    //     name: "CAF Champions League",
-    //     link: "caf-champions-league",
-    //     url: "https://www.youtube.com/c/CAFTVafricanfootball",
-    //     playlistId: "PL79m9Jm7_jmDlvHOd7miNJjDwjlq3TSLw",
-    //     videos: [],
-    //     reverseOrder: true
-    // },
-    // { 
-    //     name: "CAF Confederations Cup",
-    //     link: "caf-confederations-cup",
-    //     url: "https://www.youtube.com/c/CAFTVafricanfootball",
-    //     playlistId: "PL79m9Jm7_jmC5ZHhUcdMJDprY4rMNVIGt",
-    //     videos: [],
-    //     reverseOrder: true
-    // }
+    }*/
+    /*{ 
+        name: "CONCACAF Champions League",
+        link: "concacaf-champions-league",
+        url: "https://www.youtube.com/c/concacaf",
+        playlistId: "PL6XTKrlgbQUBLDRjeCB4NLl0oqzTzODJV",
+        videos: [],
+        reverseOrder: false
+    },*/
+    /*{ 
+        name: "CAF Champions League",
+        link: "caf-champions-league",
+        url: "https://www.youtube.com/c/CAFTVafricanfootball",
+        playlistId: "PL79m9Jm7_jmDlvHOd7miNJjDwjlq3TSLw",
+        videos: [],
+        reverseOrder: true
+    },*/
+    /*{ 
+        name: "CAF Confederations Cup",
+        link: "caf-confederations-cup",
+        url: "https://www.youtube.com/c/CAFTVafricanfootball",
+        playlistId: "PL79m9Jm7_jmC5ZHhUcdMJDprY4rMNVIGt",
+        videos: [],
+        reverseOrder: true
+    }*/
 ];
